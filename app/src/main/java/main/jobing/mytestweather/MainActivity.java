47599fd670f81085ae40b12d2bb2e30b476b69a4
Dmitry Kustov <dlkustov@gmail.com>
2020-07-18 11:08:14 +0300

    Second Import

--- app/src/main/java/main/jobing/mytestweather/MainActivity.java
+++ app/src/main/java/main/jobing/mytestweather/MainActivity.java
@@ -21,14 +21,7 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    /**
-     * Для простоты и удобства используем уже сформированную строку
-     * с запросом погоды в Лондоне на данный момент
-     * <p>
-     * другие примеры запросов можете глянуть здесь
-     * {@see <a href="http://openweathermap.org/current">openweathermap</a>}
-     * также Вам понадобится свой API ключ appid
-     */
+
     URL url = null;
     public static final String WEATHER_URL =
             "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=241de9349721df959d8800c12ca4f1f3&lang=ru";
